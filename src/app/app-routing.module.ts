a3661924315acc4abe57a46fe2bde39337163b69
ant1ge <lorenzo.petrini92@gmail.com>
2017-12-13 11:07:19 +0100

    v. 0.0.4 - Login, register e controlli *CORRETTO*

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {ProductComponent} from "./product/product.component";
+import {AuthGuard} from "./auth-guard.service";
+
+
+const routes: Routes =[
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'product', component: ProductComponent, canActivate: [AuthGuard]}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {LoginService} from "../login.service";
+import {Router} from "@angular/router";
+import {SharedService} from "../shared.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user = {username: '', password: ''};
+
+  constructor(private loginService: LoginService, private router: Router,
+              private _sharedService: SharedService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.loginService.login(this.user).subscribe(data => {
+      console.log(data);
+      localStorage.setItem('user', JSON.stringify(data));
+      localStorage.setItem('token', btoa(this.user.username + ':' + this.user.password));
+      this._sharedService.emitChange('logged=true');
+      this.router.navigate(['listproduct'], { replaceUrl: true });
+    }, err => {
+      console.log(err);
+    })
+  }
+
+}
