e0a8f8e9b37b203261799fe5e39650015532983a
ant1ge <lorenzo.petrini92@gmail.com>
2017-12-13 11:05:26 +0100

    v. 0.0.4 - Login, register e controlli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import {AuthGuard} from "./auth-guard.service";
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material';
 import { HomepageComponent } from './homepage/homepage.component';
 import {Ng2CarouselamosModule} from 'ng2-carouselamos';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AppRoutingModule } from './/app-routing.module';
+import {SharedService} from "./shared.service";
+import {InterceptorService} from "./interceptor.service";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {LoginService} from "./login.service";
+import { ProductComponent } from './product/product.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     MatIconModule,
-    Ng2CarouselamosModule
+    Ng2CarouselamosModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true,
+    },
+    AuthGuard,
+    SharedService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
